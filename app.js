a5ca181c370b789a07307d3e673f9d4236eb52da
Aaron Bieber <deftly@gmail.com>
2013-09-24 16:18:52 -0600

    openshift

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var express = require('express');
+var express = require('express'),
+app = express(),
+port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.logger());
+
+app.engine('html', require('ejs').renderFile);
+
+app.listen(port, host);
