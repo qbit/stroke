80850309a3aa98c9bc3c9d0511452937d38de2a1
Aaron Bieber <deftly@gmail.com>
2013-09-24 17:10:11 -0600

    stupid submodules

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 app.use(express.static(__dirname + '/public'));
 app.use(express.logger());
 
+app.set('view engine', 'ejs');
+
 app.engine('html', require('ejs').renderFile);
+
+app.locals.title = 'Stroke';
+app.locals.users = 0;
+app.locals.version = '0.0.1';
+
+app.get('/', function(req,res) {
+	res.render('index');
+});
 
 app.listen(port, host);
